ebd93149c6be8a0c3ca53b7bf8702d2318d514f1
Dmither <mr.d.graumann@gmail.com>
2023-04-04 14:23:29 +0300

    update

--- index.js
+++ index.js
@@ -1,82 +1,27 @@
-function pow(x, n) {
-	let result = 1;
-
-	for (let i = 0; i < n; i++) {
-		result *= x;
-	}
-
-	return result;
+let user = {
+  name: "John",
+  age: 26,
+  [Symbol.toPrimitive](hint) {
+    console.log(`hint: ${hint}`);
+    return hint == "string" ? this.name : this.age;
+  },
 }
 
-// let menu = {
-//   width: 200,
-//   height: 300,
-//   title: "Моє меню"
-// };
+console.log(user);
+console.log("" + user);
+console.log(+user);
 
-// console.log(menu);
-
-// let copy = {};
-
-// for (const key in menu) {
-//   copy[key] = menu[key];
-// }
-
-// console.log(copy);
-
-// let copy1 = Object.assign({}, menu);
-
-// console.log(copy1);
-// console.log(typeof copy);
-
-// function cloneObj(obj) {
-// 	let res = {};
-
-// 	for (let key in obj) {
-// 		res[key] =
-// 			typeof obj[key] == "object" ? cloneObj(obj[key]) : obj[key];
-// 	}
-
-// 	return res;
-// }
-
-// let person = {
-// 	name: "Sam",
-// 	age: 26,
-// 	parents: {
-// 		father: "Tom",
-// 		mother: "Alice",
-// 	},
-// 	sayHi() {
-// 		console.log(`Hi! I am ${this.name}`);
-// 	},
-// };
-
-// console.log(person);
-
-// let anotherPerson = cloneObj(person);
-
-// console.log(anotherPerson);
-// anotherPerson.name = "Rick";
-// anotherPerson.parents.mother = "Nina";
-
-// anotherPerson.sayHi();
-
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
+let person = {
+  name: "John",
+  age: 26,
+  toString() {
+    return this.name;
   },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function (){ // показує поточний крок
-    alert( this.step );
-    return this;
+  valueOf() {
+    return this.age;
   }
-};
+}
 
-
-ladder.up().up().showStep()
+console.log(person);
+console.log("" + person);
+console.log(+person);
