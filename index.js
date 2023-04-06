688ee40de0bf98c7e43feebe7b3fd690df9ad37f
Dmither <mr.d.graumann@gmail.com>
2023-04-06 14:28:42 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-let str = "Hello!";
+// function randomDouble(min, max) {
+//   return Math.random() * (max - min) + min
+// }
 
-let num = 555..toFixed(2);
-console.log(num)
+// function randomInteger(min, max) {
+//   return Math.floor(randomDouble(min, max));
+// }
+
+// function checkSpam(str) {
+//   str = str.toLowerCase()
+//   let flag = false;
+//   if (str.includes("viagra") || str.includes("xxx")) {
+//     flag = true;
+//   }
+//   return flag;
+// }
+
+// function truncate(str, len) {
+//   if (str.length > len) {
+//     return (len > 4 ? str.slice(0, len - 3) : str.slice(0, 2)) + "..."
+//   }
+//   return str
+// }
