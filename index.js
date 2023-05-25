4c75ee7dd807eb9decb210b4d6cf683c5d211cba
Dmither <mr.d.graumann@gmail.com>
2023-05-25 17:09:18 +0300

    update

--- index.js
+++ index.js
@@ -17,3 +17,37 @@ const bmw = new Auto("BMW", "100,000", 100);
 const nissan = new Auto("Nissan", "100,000", 100);
 
 // ===================================================================
+
+let user = {
+	name: "John",
+	surname: "Smith",
+
+	set fullName(value) {
+		[this.name, this.surname] = value.split(" ");
+	},
+
+	get fullName() {
+		return `${this.name} ${this.surname}`;
+	},
+};
+
+let admin = {
+	__proto__: user,
+	isAdmin: true,
+};
+
+admin.fullName = "Alice Cooper";
+
+let ours = [];
+let extanded = [];
+
+for (let prop in admin) {
+	if (admin.hasOwnProperty(prop)) {
+		ours.push(prop);
+	} else {
+		extanded.push(prop);
+	}
+}
+
+// ===================================================================
+
