57597e47775f2995a9bcf35ec03143590c16bb28
Dmither <mr.d.graumann@gmail.com>
2023-06-02 17:47:34 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-function groupById(arr) {
-  return arr.reduce((obj, item) => {
-    obj[item.id] = item;
-    return obj;
-  }, {})
-}
-
-let users = [
-  {id: 'іван', name: "Іван Іванко", age: 20},
-  {id: 'ганна', name: "Ганна Іванко", age: 24},
-  {id: 'петро', name: "Петро Петренко", age: 31},
-];
-
-let usersById = groupById(users);
-
-console.log(usersById)
+update
