2d94c35cd03251d192e6645df7039a575a94a868
Dmither <mr.d.graumann@gmail.com>
2023-06-12 15:37:42 +0300

    update

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-async function func() {
-	let promise = new Promise((res, rej) => {
-		res(1);
-	});
-	promise.then(console.log);
-}
+async function* generateSequence(start, end) {
+	for (let i = start; i <= end; i++) {
+		await new Promise(resolve => setTimeout(resolve, 1000));
 
-func();
-
-async function func2() {
-	let promise = new Promise((res, rej) => {
-		res(1);
-	});
-	console.log(await promise);
-}
-func2();
-
-// =============================================
-
-async function loadJson(url) {
-  let response = await fetch(url);
-	if (response.status == 200) {
-		return await response.json();
+		yield i;
 	}
-	
-	throw new Error(response.status);
-
 }
 
-loadJson('https://javascript.info/no-such-user.json')
-  .catch(alert); // Error: 404
+(async () => {
+	let generator = generateSequence(1, 5)
+	for await (let value of generator) {
+		console.log(value);
+	}
+})();
