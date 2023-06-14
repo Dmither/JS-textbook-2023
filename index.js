caa90df6b426af322df169bff0d1788e45328328
Dmither <mr.d.graumann@gmail.com>
2023-06-14 13:41:29 +0300

    udpate

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-async () => {
-	let {one} = await import("./outer.js")
-}
-import { config, sayHi } from "./outer.js";
-
-console.log("  < main ============================== >")
-
-config.user = "Peter";
-
-import { two } from "./outer2.js";
-import User from "./User.js";
-sayHi();
-
-
-console.log("  </ main ============================= >")
+let operation = prompt();
+console.log(eval(operation))
